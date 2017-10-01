cd5bb420ba8a4081fa41d52aa68c276cf4df10d9
Abylai <erlanulyabylai@gmail.com>
2017-10-01 07:37:24 +0600

    expert1 бутагында Hellojava MyMath га озгерситер енгиздик

--- .idea/src/Git/lab/Hello.java
+++ .idea/src/Git/lab/Hello.java
@@ -5,5 +5,11 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("Hello BROO");
         System.out.println("Hiiiiiiiii");
+        int a=MyMath.kvadrat(5);
+        int b=MyMath.kub(5);
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println(MyMath.max(8,9));
     }
 }
--- .idea/src/Git/lab/MyMath.java
+++ .idea/src/Git/lab/MyMath.java
@@ -1,9 +1,27 @@
 package Git.lab;
 public class MyMath {
     public static int kvadrat(int a) {
-        return a*a;
+        return a * a;
     }
-    public static  int kub(int a){
-    return a*a*a;
-}
+
+    public static int kub(int a) {
+        return a * a * a;
+    }
+    public static boolean isEven (int a) {
+        return (a % 2 == 0);
+    }
+
+    public static int max(int a, int b) {
+        if (a >= b)
+            return a;
+        else
+            return b;
+    }
+
+    public static int min(int a, int b) {
+        if (a >= b)
+            return b;
+        else
+            return a;
+    }
 }
