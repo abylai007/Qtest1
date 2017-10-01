e1c9b621adba45f4b95dd4d752469fb64d454f86
Abylai <erlanulyabylai@gmail.com>
2017-10-01 07:59:11 +0600

    expert1 бутагында Hellojava озгерситер енгиздик

--- .idea/src/Git/lab/Hello.java
+++ .idea/src/Git/lab/Hello.java
@@ -4,12 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello BROO");
-        System.out.println("Hiiiiiiiii");
-        int a=MyMath.kvadrat(5);
-        int b=MyMath.kub(5);
 
-        System.out.println("a="+a);
-        System.out.println("b="+b);
-        System.out.println(MyMath.max3(8,9,12));
+        System.out.println("max="+MyMath.max3(8,9,12));
+        System.out.println("min="+MyMath.min3(12,8,15));
     }
 }
