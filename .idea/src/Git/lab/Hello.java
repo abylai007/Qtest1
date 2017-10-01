69ebfc74848998c725e3eb843117c186d16f1164
Abylai <erlanulyabylai@gmail.com>
2017-10-01 07:03:30 +0600

    Hello java файлына озгерис енгиздик

--- .idea/src/Git/lab/Hello.java
+++ .idea/src/Git/lab/Hello.java
@@ -3,5 +3,7 @@ package Git.lab;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello BROO");
+        System.out.println("Hiiiiiiiii");
     }
 }
